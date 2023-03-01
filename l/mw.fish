cf03d5cbdb9ba9e44085e8a97fff8bbf37031925
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-03-01 20:52:30 +0000

    🌆

--- l/mw.fish
+++ l/mw.fish
@@ -1,9 +1,16 @@
-function mw
-    for i in (seq 1 $argv)
-        echo ""
-        echo "Wallet Number:" $i 
-        echo "--------------"
-        echo ""
-        cast wallet new
-        end
-end
+#!/bin/fish 
+echo "Command for making wallets"
+echo ""
+echo "**************************************************"
+echo ""
+echo "How Many wallets to create ? - txt File will be created with the input number"
+read -l wax
+echo ""
+echo "**************************************************"
+for i in (seq 1 $wax)
+    echo ""
+    echo "---------------------------------------"
+    echo "New Wallet $i"
+    cast w n 
+    echo "---------------------------------------"
+end &> $w.w.w
